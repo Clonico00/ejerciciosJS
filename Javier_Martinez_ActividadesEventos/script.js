5ddd6dd20e95783eb7b9b993a12502e6bdc618ca
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-07 11:02:46 +0100

    ejercicio eventos terminado

--- Javier_Martinez_ActividadesEventos/script.js
+++ Javier_Martinez_ActividadesEventos/script.js
@@ -14,3 +14,36 @@ window.addEventListener("load", function () {
     var fecha = new Date();
     console.log("Tiempo transcurrido desde la carga de la página: " + fecha.getTime());
 });
+//Ejercicio 4
+//----------------------------------------------------------------------------------------------------------------------
+function pintarCelda(celda) {
+    if (event.ctrlKey) {
+        celda.style.backgroundColor = "red";
+    } else if (event.shiftKey) {
+        celda.style.backgroundColor = "yellow";
+    } else {
+        celda.style.backgroundColor = "white";
+    }
+}
+
+window.onload = function () {
+    var tabla = document.createElement("table");
+    tabla.setAttribute("border", "1");
+    tabla.setAttribute("id", "tabla");
+    for (var i = 0; i < 10; i++) {
+        var fila = document.createElement("tr");
+        for (var j = 0; j < 10; j++) {
+            var celda = document.createElement("td");
+            celda.setAttribute("width", "50");
+            celda.setAttribute("height", "50");
+            celda.setAttribute("id", "celda" + i + j);
+            celda.setAttribute("onmousemove", "pintarCelda(this)");
+            fila.appendChild(celda);
+        }
+        tabla.appendChild(fila);
+    }
+    document.body.appendChild(tabla);
+}
+
+
+
