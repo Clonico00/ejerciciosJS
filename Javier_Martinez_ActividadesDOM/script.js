86f8bd59788b3f87d023bb2d2c5e4ec074b3ee0f
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2022-11-11 09:51:04 +0100

    ejercicio DOM

---
+++ Javier_Martinez_ActividadesDOM/script.js
@@ -0,0 +1,28 @@
+//Ejercicio 1
+//----------------------------------------------------------------------------------------------------------------------
+/*1. Crea una página web con al menos tres párrafos de texto y dentro de ellos un número
+indeterminado de enlaces (al menos uno debe enlazar a google). Haciendo tan sólo uso del DOM
+muestra los siguientes datos:
+• Número de enlaces de la página
+• Dirección a la que enlaza el penúltimo enlace
+• Numero de enlaces que enlazan a google
+• Número de enlaces del tercer párrafo*/
+
+//Ejercicio 4
+//----------------------------------------------------------------------------------------------------------------------
+/*4. Crea una web con una lista de elementos y un botón que cada vez que pulsemos introduzca
+una entrada nueva en la lista.*/
+
+//Ejercicio 5
+//----------------------------------------------------------------------------------------------------------------------
+/*5. Estás diseñando una aplicación web para subir ficheros al servidor. Haciendo uso de DOM,
+consigue que el usuario pueda adjuntar tantos ficheros como desee. Por tanto inicialmente será
+necesario un input de tipo fichero, un botón “Adjuntar otro fichero” y un segundo botón
+“Enviar”, de modo que al pulsar el primero de los botones automáticamente aparezca otro input
+para elegir fichero.*/
+
+//Ejercicio 6
+//----------------------------------------------------------------------------------------------------------------------
+/*6. Se desea realizar mediante DOM un visor de imágenes. Para ello en tu página se mostrará una
+imagen, junto a dos botones “Siguiente” y “Anterior”. Impleméntala la funcionalidad para poder
+navegar entre un conjunto de imágenes que tendrás previamente preparadas*/
