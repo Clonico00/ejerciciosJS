4d84f86f6fdcff5a77ff0b66900f708ac5008a02
Javier Martinez Garcia <javier.mgarcia02@gmail.com>
2023-02-13 14:08:29 +0100

    proyecto JQuery

--- UD8/proyectoJQuery/script.js
+++ UD8/proyectoJQuery/script.js
@@ -1,100 +1,217 @@
-$(document).ready(function() {
-    $("#name").blur(function() {
-        validateName();
-    });
-
-    $("#phone").blur(function() {
-        if (validateName()) {
-            validatePhone();
-        }
-    });
-
-    $("#dni").blur(function() {
-        if (validateName() && validatePhone()) {
-            validateDni();
-        }
-    });
-
-    $("#text").blur(function() {
-        if (validateName() && validatePhone() && validateDni()) {
-            validateText();
-        }
-    });
-
-    $("#date").blur(function() {
-        if (validateName() && validatePhone() && validateDni() && validateText()) {
-            validateDate();
-        }
-    });
-
+$(document).ready(function () {
+    // Función de validación para el nombre
     function validateName() {
         var name = $("#name").val();
-        var nameReg = /^[a-zA-Z]+$/;
-        if (!nameReg.test(name)) {
-            $("#name").addClass("error");
-            $("#name").after("<span class='error-message'>El nombre solo puede contener letras</span>");
-            return false;
-        } else {
-            $("#name").removeClass("error");
-            $(".error-message").remove();
-            return true;
-        }
+        return name.trim() != "";
     }
 
+    // Función de validación para el teléfono
     function validatePhone() {
         var phone = $("#phone").val();
-        var phoneReg = /^\d{9}$/;
-        if (!phoneReg.test(phone)) {
-            $("#phone").addClass("error");
-            $("#phone").after("<span class='error-message'>El teléfono debe tener 9 dígitos</span>");
-            return false;
-        } else {
-            $("#phone").removeClass("error");
-            $(".error-message").remove();
-            return true;
-        }
+        var phonePattern = /^\d{9}$/;
+        return phonePattern.test(phone);
     }
 
+    // Función de validación para el DNI
     function validateDni() {
         var dni = $("#dni").val();
-        var dniReg = /^\d{8}[a-zA-Z]$/;
-        if (!dniReg.test(dni)) {
-            $("#dni").addClass("error");
-            $("#dni").after("<span class='error-message'>El DNI debe tener 8 números y una letra</span>");
-            return false;
-        } else {
-            $("#dni").removeClass("error");
-            $(".error-message").remove();
-            return true;
-        }
+        var dniPattern = /^\d{8}[A-Za-z]$/;
+        return dniPattern.test(dni);
     }
 
+    // Función de validación para el texto
     function validateText() {
         var text = $("#text").val();
-        var textReg = /^[a-zA-Z0-9]+$/;
-        if (!textReg.test(text)) {
-            $("#text").addClass("error");
-            $("#text").after("<span class='error-message'>El texto solo puede contener letras y números</span>");
-            return false;
-        } else {
-            $("#text").removeClass("error");
-            $(".error-message").remove();
-            return true;
-            
-        }
+        return text.trim() != "";
     }
-    
+
+    // Función de validación para la fecha
     function validateDate() {
         var date = $("#date").val();
-        var dateReg = /^\d{2}\/\d{2}\/\d{4}$/;
-        if (!dateReg.test(date)) {
-            $("#date").addClass("error");
-            $("#date").after("<span class='error-message'>La fecha debe tener el formato dd/mm/aaaa</span>");
-            return false;
+        return date != "";
+    }
+
+    // Evento de escucha para el nombre
+    $("#name").on("blur", function () {
+        var isValid = validateName();
+        if (!isValid) {
+            alert("Por favor ingrese su nombre");
+            $("#name").focus();
+            //desactivamos los demas campos
+            $("#phone").prop("disabled", true);
+            $("#dni").prop("disabled", true);
+            $("#text").prop("disabled", true);
+            $("#date").prop("disabled", true);
+            //si esta bien los activamos
         } else {
-            $("#date").removeClass("error");
-            $(".error-message").remove();
-            return true;
+            $("#phone").prop("disabled", false);
+            $("#dni").prop("disabled", false);
+            $("#text").prop("disabled", false);
+            $("#date").prop("disabled", false);
+
+        }
+    });
+
+    // Evento de escucha para el teléfono
+    $("#phone").on("blur", function () {
+        var isValid = validatePhone();
+        if (!isValid) {
+            alert("Por favor ingrese un número de teléfono válido (9 dígitos)");
+            $("#phone").focus();
+            //desactivamos los demas campos
+            $("#dni").prop("disabled", true);
+            $("#text").prop("disabled", true);
+            $("#date").prop("disabled", true);
+            //si esta bien los activamos
+        } else {
+            $("#dni").prop("disabled", false);
+            $("#text").prop("disabled", false);
+            $("#date").prop("disabled", false);
+
+        }
+    });
+
+    // Evento de escucha para el DNI
+    $("#dni").on("blur", function () {
+        var isValid = validateDni();
+        if (!isValid) {
+            alert("Por favor ingrese un DNI válido");
+            $("#dni").focus();
+            //desactivamos los demas campos
+            $("#text").prop("disabled", true);
+            $("#date").prop("disabled", true);
+            //si esta bien los activamos
+        } else {
+            $("#text").prop("disabled", false);
+            $("#date").prop("disabled", false);
+
+        }
+    });
+
+    // Evento de escucha para el texto
+    $("#text").on("blur", function () {
+        var isValid = validateText();
+        if (!isValid) {
+            alert("Por favor ingrese un texto");
+            $("#text").focus();
+            //desactivamos los demas campos
+            $("#date").prop("disabled", true);
+            //si esta bien los activamos
+        } else {
+            $("#date").prop("disabled", false);
+
+
+        }
+    });
+
+    // Evento de escucha para la fecha
+    $("#date").on("blur", function () {
+        var isValid = validateDate();
+        if (!isValid) {
+            alert("Por favor seleccione una fecha");
+            $("#date").focus();
+        }
+    });
+});
+
+//---------------------------------------------------------------------------------------------------------------------
+
+
+// Muestra los testimonios en formato de tabla
+$.ajax({
+    url: 'servicios.json',
+    success: function (data) {
+        // Selecciona 3 testimonios aleatorios
+        let testimonios = [];
+        for (let i = 0; i < 3; i++) {
+            let testimonio = data.testimonials[Math.floor(Math.random() * data.testimonials.length)];
+            testimonios.push(testimonio);
+        }
+
+        // Muestra los testimonios en formato original
+        let testimoniosContainer = $('#testimonios .testimonios-card-container');
+        let view = 'default';
+        loadTestimonials();
+
+        // Eventos para alternar entre vistas
+        $('#btn-tabla').click(function () {
+            view = 'table';
+            showTestimonios();
+        });
+        $('#btn-default').click(function () {
+            view = 'default';
+            showTestimonios();
+        });
+
+        function showTestimonios() {
+            testimoniosContainer.empty();
+            if (view === 'default') {
+                testimonios.forEach(function (testimonio) {
+                    testimoniosContainer.append(`
+                        <div class="testimonio-card">
+                            <h3>${testimonio.name}</h3>
+                            <p>${testimonio.text}</p>
+                            <span>${testimonio.date}</span>
+                        </div>
+                    `);
+                });
+            } else {
+                testimoniosContainer.append(`
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Texto</th>
+                                <th>Fecha</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                `);
+                testimonios.forEach(function (testimonio) {
+                    testimoniosContainer.append(`
+                        <tr>
+                            <td>${testimonio.name}</td>
+                            <td>${testimonio.text}</td>
+                            <td>${testimonio.date}</td>
+                        </tr>
+                    `);
+                });
+                testimoniosContainer.append(`
+                        </tbody>
+                    </table>
+                `);
+            }
+        }
+
+        function loadTestimonials() {
+            $.ajax({
+                url: 'servicios.json',
+                success: function (data) {
+                    let testimonios = [];
+                    for (let i = 0; i < 3; i++) {
+                        let testimonio = data.testimonials[Math.floor(Math.random() * data.testimonials.length)];
+                        testimonios.push(testimonio);
+                    }
+                    let testimoniosContainer = $('#testimonios .testimonios-card-container');
+                    testimonios.forEach(function (testimonio) {
+                        testimoniosContainer.append(`
+        <div class="testimonio-card">
+          <h3>${testimonio.name}</h3>
+          <p>${testimonio.text}</p>
+          <span>${testimonio.date}</span>
+        </div>
+        `);
+                    });
+                },
+                error: function (error) {
+                    setTimeout(loadTestimonials, 5000);
+                }
+            });
         }
     }
 });
+
+
+
+
+
